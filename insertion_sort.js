04978c4252a83a373f4cc7ef252c648e09f0f7e9
Roger-Aguiar <roger.silvaaguiar@yahoo.com.br>
2021-08-10 11:38:28 -0300

    It implements the sortAscending and displayArray methods.

---
+++ insertion_sort.js
@@ -0,0 +1,44 @@
+class InsertionSort
+{
+    constructor()
+    {
+        this.arrange = [5, 2, 4, 6, 1, 3, 9, 7, 8, 0, 10, 18, 13];
+    }
+
+    sortAscending()
+    {
+        for(let i = 0; i < Object.keys(this.arrange).length - 1; i++)
+        {            
+            let j = i + 1;
+            let auxIndex = i;
+            let key = this.arrange[j];
+            while(j > 0)
+            {
+                if(key < this.arrange[auxIndex])
+                {
+                    let aux = this.arrange[auxIndex];
+                    this.arrange[auxIndex] = key;
+                    this.arrange[j] = aux;                    
+                }
+                auxIndex--;
+                j--;
+            }
+            this.displayArray();
+        }
+    }
+
+    displayArray()
+    {
+        let output = "";
+
+        for(let i = 0; i < Object.keys(this.arrange).length; i++)
+        {
+            output+= this.arrange[i] < 10 ? "0" + this.arrange[i] + "   " : this.arrange[i] + "   ";            
+        }
+        console.log(output);
+    }
+}
+
+let insertion = new InsertionSort();
+insertion.displayArray();
+insertion.sortAscending();
